062b026214c29da107ef9073d371f784243b91b6
Jay Levy <jayelevy@gmail.com>
2022-07-31 22:47:40 -0500

    Initial Commit

---
+++ ExampleDeleteFailure/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ExampleDeleteFailure
+//
+//  Created by Jay Levy on 7/31/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ExampleDeleteFailure/ExampleDeleteFailureApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleDeleteFailureApp.swift
+//  ExampleDeleteFailure
+//
+//  Created by Jay Levy on 7/31/22.
+//
+
+import SwiftUI
+
+@main
+struct ExampleDeleteFailureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
